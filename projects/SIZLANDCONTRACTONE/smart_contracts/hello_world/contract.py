fbad9403bb0e84eec1da1ddeea9dcfcb52ca6a3b
D3stinn3 <paynedestinne@gmail.com>
2025-05-04 23:16:39 +0300

    Contract

--- projects/SIZLANDCONTRACTONE/smart_contracts/hello_world/contract.py
+++ projects/SIZLANDCONTRACTONE/smart_contracts/hello_world/contract.py
@@ -1,8 +1,67 @@
-from algopy import ARC4Contract, String
-from algopy.arc4 import abimethod
+from algopy import Account, ARC4Contract, Asset, BoxMap, Global, Txn, arc4, itxn
+from algopy.arc4 import Struct
 
 
-class HelloWorld(ARC4Contract):
-    @abimethod()
-    def hello(self, name: String) -> String:
-        return "Hello, " + name
+class UserAllocation(Struct):
+    total_allocation: arc4.UInt64
+    claimed_amount: arc4.UInt64
+    start_time: arc4.UInt64
+    cliff_time: arc4.UInt64
+    vesting_period: arc4.UInt64
+    last_claim_time: arc4.UInt64
+
+
+class SizlandVestingContract(ARC4Contract):
+    # Define state variables for vesting  parameters
+    # For example: beneficiary, total amount, vesting period, etc.
+
+    def __init__(self) -> None:
+        # Initialize your contract state
+        # This will be executed once on deployment
+        self.asa = Asset()
+        self.allocations = BoxMap(Account, UserAllocation, key_prefix="")
+
+    @arc4.abimethod
+    def opt_into_asset(self, asset: Asset) -> None:
+        # Only allow app creator to opt the app account into a ASA
+        assert Txn.sender == Global.creator_address, "Only creator can opt in to ASA"
+        # Verify a ASA hasn't already been opted into
+        assert self.asa.id == 2905622564, "ASA already opted in"
+        # Save ASA ID in global state
+        self.asa = asset
+
+        # Submit opt-in transaction: 0 asset transfer to self
+        itxn.AssetTransfer(
+            asset_receiver=Global.current_application_address,
+            xfer_asset=asset,
+        ).submit()
+
+    @arc4.abimethod
+    def set_allocation(
+        self,
+        beneficiary: Account,
+        total_allocation: arc4.UInt64,
+        start_time: arc4.UInt64,
+        cliff_time: arc4.UInt64,
+        vesting_period: arc4.UInt64,
+    ) -> None:
+        assert (
+            Txn.sender == Global.creator_address
+        ), "Only the creator can call this method"
+        self.allocations[beneficiary] = UserAllocation(
+            total_allocation=total_allocation,
+            claimed_amount=arc4.UInt64(0),
+            start_time=start_time,
+            cliff_time=cliff_time,
+            vesting_period=vesting_period,
+            last_claim_time=arc4.UInt64(0),
+        )
+
+    @arc4.abimethod
+    def claim(self) -> None:
+        current_time = arc4.UInt64(Global.latest_timestamp)
+
+        sender = Txn.sender
+
+        allocation, exists = self.allocations.maybe(sender)
+        assert exists, "No allocation found"
